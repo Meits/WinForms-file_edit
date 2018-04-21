b31b99e9c03f512daa01a1d87845c6405906ad0f
Viktor <meitslin@gmail.com>
2018-04-22 00:44:22 +0300

    Add file edit

---
+++ dz3-3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz3_3
+{
+    public partial class Form1 : Form
+    {
+        public StreamReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            open.FilterIndex = 1;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                this.reader = File.OpenText(open.FileName);
+                this.textUpdate(this.reader.ReadToEnd());
+                button2.Enabled = true;
+                
+            }
+        }
+
+        public void textUpdate(string text)
+        {
+            textBox1.Text = text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2(this, textBox1.Text);
+            f.Show();
+        }
+    }
+}
---
+++ dz3-3/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz3_3
+{
+    public partial class Form2 : Form
+    {
+        Form1 form;
+        public Form2(Form1 form, string reader)
+        {
+            InitializeComponent();
+            textBox1.Text = reader;
+            this.form = form;
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+
+            //создали экземпляр
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(save.FileName);
+                //записываем в файл содержимое поля
+                writer.Write(textBox1.Text); writer.Close();
+                //закрываем writer
+            }
+
+            form.textUpdate(textBox1.Text);
+            this.Close();
+        }
+
+    }
+}
